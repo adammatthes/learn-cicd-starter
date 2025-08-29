9832209beab57204746d6f10bea66c448478a40c
adammatthes <adammatthes@gmail.com>
2025-08-29 11:25:17 -0400

    Add test for auth; purposely broke one test to observe failure in CI-CD

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	testHeader := http.Header{}
+
+	testHeader.Set("Authorization", "ApiKey 12345")
+
+	_, err := GetAPIKey(testHeader)
+	if err == nil {
+		t.Errorf("Failed to extract API key with correct format: %s", err)
+	}
+
+	testHeader.Set("Authorization", "12345")
+	_, err = GetAPIKey(testHeader)
+	if err == nil {
+		t.Errorf("Error checking did not catch malformed authorization" )
+	}
+
+	testHeader.Set("Authorization", "")
+	_, err = GetAPIKey(testHeader)
+	if err == nil {
+		t.Errorf("Error checking did not stop empty authorization")
+	}
+}
