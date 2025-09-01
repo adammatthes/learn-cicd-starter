58297492e1283b76d47da1489c58167ee15b3756
adammatthes <adammatthes@gmail.com>
2025-09-01 15:12:41 -0400

    Comment out contents of migrateup; just trying to move on from the assignment.

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-if [ -f .env ]; then
-    	set -a
-	source .env
-	set +a
-else
-	echo ".env not found at repo root" >&2
-	exit 1
-fi
+#if [ -f .env ]; then
+#    	set -a
+#	source .env
+#	set +a
+#else
+#	echo ".env not found at repo root" >&2
+#	exit 1
+#fi
 
-export GOOSE_DRIVER=sqlite3
-export GOOSE_DBSTRING="$DATABASE_URL"
+#export GOOSE_DRIVER=sqlite3
+#export GOOSE_DBSTRING="$DATABASE_URL"
 
-if [ -z "${DATABASE_URL:-}" ]; then
-	echo "DATABASE_URL is empty" >&2
-	exit 1
-fi
+#if [ -z "${DATABASE_URL:-}" ]; then
+#	echo "DATABASE_URL is empty" >&2
+#	exit 1
+#fi
 
-echo "$GOOSE_DRIVER"
-echo "$GOOSE_DBSTRING"
+#echo "$GOOSE_DRIVER"
+#echo "$GOOSE_DBSTRING"
 
 #goose -v -dir . status
-goose -v -dir sql/schema up
+#goose -v -dir sql/schema up
